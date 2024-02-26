55bf3ef267edbeabd964a26553a50603a6f88c37
Renan Pollo Benelli <renansk18@outlook.com>
2024-02-26 20:19:59 -0300

    Feat: Final adjusts

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -8,22 +10,37 @@ import { GetUnitsService } from 'src/app/services/get-units.service';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-  results = [];
-  form!: FormGroup;
+  @Output() submitEvent = new EventEmitter();
+  results: Location[] = [];
+  filteredResults: Location[] = [];
+  formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService) { }
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(
-      data => console.log(data)
-    );
-    this.form = this.formBuilder.group({
+    this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
+    })
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data;
+      this.filteredResults = data;
     });
   }
 
   onSubmit(): void {
-    console.log(this.form)
+    let { showClosed, hour } = this.formGroup.value
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
   }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
+
 }
--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,17 +1,35 @@
-import { UnitsResponse } from './../types/units-response.interface';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UnitsResponse } from '../types/units-reponse.interface';
+import { Location } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetUnitsService {
-
-  constructor(private http: HttpClient) { }
   readonly apiUrl = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json";
 
-  getAllUnits(): Observable<UnitsResponse> {
-    return this.http.get<UnitsResponse>(this.apiUrl);
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
+  private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<UnitsResponse>(this.apiUrl).subscribe(data => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations;
+    });
+  }
+
+  getAllUnits(): Observable<Location[]> {
+    return this.allUnits$;
+  }
+
+  getFilteredUnits() {
+    return this.filteredUnits;
+  }
+
+  setFilteredUnits(value: Location[]) {
+    this.filteredUnits = value;
   }
 }
